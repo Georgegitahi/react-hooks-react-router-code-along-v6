dc42c6343e119c0984a76e3f5bb356d1a859cef2
Georgegitahi <petergeorgembugua724@gmail.com>
2024-05-05 13:07:54 +0300

    lab complete

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+ function About() {
+  return (
+    <>
+    <header>
+
+    </header>
+    <main>
+        <h1>This is my about component</h1>
+    </main>
+    </>
+  )
+}
+export default About;
